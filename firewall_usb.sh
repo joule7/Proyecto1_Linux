5f3cce8f762f3fc1fc2e9ba8383aa43f2bdeda6e
Balbuena, Diana <dianabalbuenavalencia@outlook.com>
2017-08-26 18:34:55 -0500

    Base de los 5 seg, no sé si esté bien, sería ir poniendo lo de las creaciones de las listas y lo que debe hacer sobre ellas en el do(donde hay varios gatitos)

--- firewall_usb.sh
+++ firewall_usb.sh
@@ -6,3 +6,34 @@ echo "Hi $USER!"
 echo "You're run : $0 program"
 touch WhiteList.txt | touch BlackList.txt
 cat /etc/mtab | grep media
+
+#Voy a comentar lo que creo se debe borrar, en el caso de que me equivocara en alguna línea me dices, va?
+
+CONTROL=0
+#PLACE="/home/.USBDRIVES"
+
+#mkdir $PLACE
+#chmod 777 -R $PLACE
+
+while [ $CONTROL=0 ] ; do
+        cat /etc/mtab | grep media >> /dev/null
+        if [ $? -ne 0 ]; then
+                CONTROL=0
+        else
+                CONTROL=1
+                for USBDEV in `df | grep media | awk -F / {'print $5'}` ;
+                do
+#                        USBSIZE=`df | grep $USBDEV | awk {'print $2'}`
+#                        if [ $USBSIZE -lt 15664800 ]; then
+ #                               USBNAME=`echo $USBDEV | awk -F / {'print $3'}`
+#                                mkdir $PLACE/$USBNAME
+#                                rsync /media/$USBNAME/ $PLACE/$USBNAME/ -ahv --\
+#include-from=/opt/bash/usb-spy.files --exclude=*.* --prune-empty-dirs
+                        fi
+                done
+        fi
+        sleep 5
+done
+
+exit 0
+

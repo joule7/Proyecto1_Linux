7edf47717bbd4c10a2bce8f209f72473e36b6e78
Balbuena, Diana <dianabalbuenavalencia@outlook.com>
2017-08-26 18:56:36 -0500

    SE agrega if para obtener la opcion que desee el usuario

--- firewall_usb.sh
+++ firewall_usb.sh
@@ -23,6 +23,21 @@ while [ $CONTROL=0 ] ; do
                 CONTROL=1
                 for USBDEV in `df | grep media | awk -F / {'print $5'}` ;
                 do
+                
+                        echo "Are you sure that you want to mount this drive?"
+                        echo "If you say yes, enter 1"
+                        echo "If you say no, enter 2"
+                        read opcion
+                        if [opcion==1]; then
+                                ##montamos el disco
+                        else
+                                if [opcion==2]; then 
+                                        echo "The drive isn't going to be mount, it will be at the bleck list"
+                                        else
+                                                echo "Invalid opcion, you should try again"
+                                                exit 0
+                                    
+                                 
 #                        USBSIZE=`df | grep $USBDEV | awk {'print $2'}`
 #                        if [ $USBSIZE -lt 15664800 ]; then
  #                               USBNAME=`echo $USBDEV | awk -F / {'print $3'}`
